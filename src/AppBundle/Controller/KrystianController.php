26f5fa7926a4fb4e8382c804d2750c6961f9d67c
Rufusan <krysstian18@gmail.com>
2017-04-08 20:42:06 +0200

    Practices with Twig
    Templates, generating routes

---
+++ src/AppBundle/Controller/KrystianController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krystian
+ * Date: 2017-04-08
+ * Time: 20:02
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class KrystianController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function showAction()
+    {
+        return new Response("Witaj na stronie głównej!");
+    }
+
+    /**
+     * @Route("/{someName}")
+     */
+    public function pageAction($someName)
+    {
+        return $this->render('default/show.html.twig', [
+            'name' => $someName
+        ]);
+
+    }
+}
