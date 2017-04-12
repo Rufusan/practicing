035c352a48b168f128b6976042d3f5e46b73ff0f
Rufusan <krysstian18@gmail.com>
2017-04-12 22:35:23 +0200

    Redirecting
    Learn how to redirect pages, creating URLs

--- src/AppBundle/Controller/KrystianController.php
+++ src/AppBundle/Controller/KrystianController.php
@@ -16,26 +16,46 @@ use Symfony\Component\HttpFoundation\Response;
 class KrystianController extends Controller
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/luckynumber", name="lucky_number")
      */
     public function LuckyController()
     {
         $number = mt_rand(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
+        return $this->render('lucky/number.html.twig',['number' => $number]);
     }
 
     /**
-     * @Route("lucky/number/{max}")
+     * @Route("lucky/number/{max}", name = "lucky_max")
      */
     public function maxNumberAction($max)
     {
         $number = mt_rand(0, $max);
 
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
+        return $this->render('lucky/number.html.twig', ['number' => $number]);
+    }
+
+    /**
+     * @Route("/", name = "homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/{slug}", name = "podstrona")
+     */
+    public function urlAction($slug)
+    {
+        //return $this->redirectToRoute('homepage');
+        //return $this->redirectToRoute('homepage', [], 301);
+        //return $this->redirectToRoute('lucky_max', ['max' => 10]);
+        //$templating = $this->get('templating');
+        //$router = $this->get('router');
+        //$mailer = $this->get('mailer');
+        
+        return $this->render('default/page.html.twig', [
+            'slug' => $slug]);
     }
 }
